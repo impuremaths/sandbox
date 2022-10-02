e4a219d92b32cdd1d5df1975d3be2e861d704e66
impuremaths <79420726+impuremaths@users.noreply.github.com>
2022-10-01 18:28:35 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const PORT = process.env.PORT || 3000;
-const PORT
 const express = require('express');
 const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
